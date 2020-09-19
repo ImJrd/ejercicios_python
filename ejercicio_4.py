a234229621606d5957b69c2565aba80398d0a735
Jordi <48893030+ImJrd@users.noreply.github.com>
2020-09-19 08:58:38 -0500

    Ejercicio 4

---
+++ ejercicio_4.py
@@ -0,0 +1,4 @@
+#Escribir un programa que pregunte el nombre del usuario en la consola y un número entero e imprima por pantalla en líneas distintas el nombre del usuario tantas veces como el número introducido.
+name_user = input("Ingrese su nombre. ")
+number = input("Ingrese un numero cualquiera. ")
+print(name_user * int(number))
