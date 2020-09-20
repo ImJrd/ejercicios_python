da547a15fdf6116f57fea16f40b505f161dd03a9
Jordi <48893030+ImJrd@users.noreply.github.com>
2020-09-20 14:11:48 -0500

    Ejercicio 4

---
+++ condicionales/ejercicio_4.py
@@ -0,0 +1,6 @@
+#Escribir un programa que pida al usuario un número entero y muestre por pantalla si es par o impar.
+numero = input("Ingrese un numero: ")
+if float(numero)%2 == 0:
+    print("El numero", numero, "es par")
+else:
+    print("El numero15", numero, "es impar")
