a4ba8c13d9aab772cc61470cf320c16b4ee86306
Jordi <48893030+ImJrd@users.noreply.github.com>
2020-09-20 19:07:27 -0500

    Ejercicio 10

---
+++ bucles/ejercicio_10.py
@@ -0,0 +1,25 @@
+#Escribir un programa que pida al usuario un número entero y muestre por pantalla si es un número primo o no.
+def isPrime(num):
+    if num < 1:
+        return False
+    elif num == 2:
+        return True
+    else:
+        for i in range(2, num):
+            if num % i == 0:
+                return False
+        return True
+
+
+def app():
+    num = int(input('Escribe un numero: '))
+    result = isPrime(num)
+
+    if result is True:
+        print('El numero es primo')
+    else:
+        print('El numero no es primo')
+
+
+if __name__ == '__main__':
+    app()
