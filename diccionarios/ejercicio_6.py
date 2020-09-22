feddddde07f8c6c3c4345e15431bf47cdf5bab8b
Jordi <48893030+ImJrd@users.noreply.github.com>
2020-09-22 13:06:03 -0500

    Ejercicio 6

---
+++ diccionarios/ejercicio_6.py
@@ -0,0 +1,9 @@
+#Escribir un programa que cree un diccionario vacío y lo vaya llenado con información sobre una persona (por ejemplo nombre, edad, sexo, teléfono, correo electrónico, etc.) que se le pida al usuario. Cada vez que se añada un nuevo dato debe imprimirse el contenido del diccionario.
+datos = {}
+r = str("Si")
+while r == "Si":
+    key = input("¿Que datos desea introducir? ")
+    valor = input(key + ": ")
+    datos[key] = valor
+    print(datos)
+    r = str(input("¿Quiere añadir más información (Si/No)? ")).title()
