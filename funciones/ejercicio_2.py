ca2bcd0446bce422859a1fb43c96e00f28fa7695
Jordi <48893030+ImJrd@users.noreply.github.com>
2020-09-22 15:53:27 -0500

    Ejercicio 2

---
+++ funciones/ejercicio_2.py
@@ -0,0 +1,7 @@
+# Escribir una función a la que se le pase una cadena < nombre > y muestre por pantalla el saludo ¡hola < nombre >!.
+def saludo (name):
+    print("¡Hola " + name + "!")
+    return
+
+nombre = "Mario"
+saludo(nombre)
