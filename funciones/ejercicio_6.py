58d5e616fbf82898c93428bb578b76c3acffa0b7
Jordi <48893030+ImJrd@users.noreply.github.com>
2020-09-23 10:52:14 -0500

    Ejercicio 6

---
+++ funciones/ejercicio_6.py
@@ -0,0 +1,6 @@
+#Escribir una función que reciba una muestra de números en una lista y devuelva su media.
+def mean(sample):
+    return sum(sample)/len(sample)
+
+print(mean([5, 2, 3, 7, 1]))
+# print(mean(1, 2, 3, 4, 5))
